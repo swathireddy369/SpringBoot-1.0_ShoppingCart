c097961d167109284579ad397d4c5ab1a1ed7448
swathireddy369 <swathiamaravadi53@gmail.com>
2024-11-24 10:57:27 -0500

    initiated

---
+++ src/test/java/com/SpringBoot/Demo/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
